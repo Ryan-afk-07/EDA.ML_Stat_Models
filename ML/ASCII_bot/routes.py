b6e70ad06e70007e7c0dc46d6b276f3651ecad70
unknown <rfong0001@gmail.com>
2025-12-28 01:30:39 +0800

    '28DecUpdate'

--- ML/ASCII_bot/routes.py
+++ ML/ASCII_bot/routes.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify, render_template, render_template_string
 from flask_socketio import SocketIO, emit
+from models import PokemonChatBot, PokemonCardBot
+
+
+bot = PokemonCardBot(api_key='your_openai_api_key_here')   
+
 
 ##all flask routes
 def register_routes(app):
     @app.route('/')
     def home():
-        return render_template('main.html')
+        return render_template('poke_list.html')
 
-    @app.route('/gen1/')
-    def gen1():
-        return render_template('gen1.html')
+    @app.route('/chat')
+    def pokechat():
+        user_msg = request.json['message']
+        reply = bot.get_response(user_msg)
+        return jsonify({'reply': reply})
